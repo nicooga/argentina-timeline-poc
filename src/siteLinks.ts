5de6d1599e65f52228a72ad6cae1db1bb56d0564
Nicolas Oga <2112.oga@gmail.com>
2026-04-20 13:19:35 -0300

    fix: Actualizar link a cuenta de Instagram

--- src/siteLinks.ts
+++ src/siteLinks.ts
@@ -1,3 +1,3 @@
 /** Perfil de Instagram del proyecto (Historia en timelines). */
 export const SITE_INSTAGRAM_URL =
-  "https://www.instagram.com/hisctorictimelines/" as const;
+  "https://www.instagram.com/historic.timelines/" as const;
